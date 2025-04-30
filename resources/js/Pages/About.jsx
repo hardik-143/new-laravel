cbb9fcc654763679a6bfe48fb17657fe88690a0a
Hardik <dhardik1430@gmail.com>
2025-04-30 22:59:47 +0530

    refactor About and Home components for consistent formatting.

--- resources/js/Pages/About.jsx
+++ resources/js/Pages/About.jsx
@@ -1,17 +1,15 @@
-import { Link } from "@inertiajs/inertia-react";
+import { Link } from '@inertiajs/inertia-react';
 
 export default function About() {
-    return (
-        <>
-            <div className="p-10 flex flex-col items-center justify-center">
-                <h1 className="text-3xl font-bold  font-nunito">
-                    Fixed Header
-                </h1>
-                <div className="h-10 w-full bg-slate-300">About Page</div>
-                <Link href="/" className="underline">
-                    Home
-                </Link>
-            </div>
-        </>
-    );
+  return (
+    <>
+      <div className="p-10 flex flex-col items-center justify-center">
+        <h1 className="text-3xl font-bold  font-nunito">Fixed Header</h1>
+        <div className="h-10 w-full bg-slate-300">About Page</div>
+        <Link href="/" className="underline">
+          Home
+        </Link>
+      </div>
+    </>
+  );
 }
--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -1,16 +1,14 @@
-import { Link } from "@inertiajs/inertia-react";
+import { Link } from '@inertiajs/inertia-react';
 
 export default function Home() {
-    let hello = "Hello";
-    return (
-        <div className="p-10 flex flex-col items-center justify-center">
-            <h1 className="text-3xl font-bold">Fixed Header</h1>
-            <div className="h-10 w-full bg-slate-300 text-[25px]">
-                {hello} Home Page
-            </div>
-            <Link href="/about" className="underline">
-                About
-            </Link>
-        </div>
-    );
+  let hello = 'Hello';
+  return (
+    <div className="p-10 flex flex-col items-center justify-center">
+      <h1 className="text-3xl font-bold">Fixed Header</h1>
+      <div className="h-10 w-full bg-slate-300 text-[25px]">{hello} Home Page</div>
+      <Link href="/about" className="underline">
+        About
+      </Link>
+    </div>
+  );
 }
